dc668deb411f655cbbf7233adace7322c026108b
Phillip WLG <wulianp@wwu.edu>
2019-11-08 18:40:11 -0800

    setting react app

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Cloud PhotoX</h1>
+        <p>A simple photo cloud storage </p>
+      </div>
+    </div>
+  );
+}
